18944b99372c0c91c868a54851a63364cedbec1d
Andrey Pyzhyanov <andreypyzhyanov87@yandex.ru>
2025-04-12 10:29:41 +0300

    1-iteration

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
-// @todo: Темплейт карточки
+function createCard(cardData, handleDelete) {
+  const cardTemplate = document.getElementById('card-template').content;
+  const cardElement = cardTemplate.cloneNode(true).querySelector('.card');
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: DOM узлы
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
 
-// @todo: Функция создания карточки
+  deleteButton.addEventListener('click', () => {
+    handleDelete(cardElement); 
+  });
 
-// @todo: Функция удаления карточки
+  return cardElement;
+}
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+const placesList = document.querySelector('.places__list');
+function renderCards(cards) {
+  cards.forEach(cardData => {
+    const cardElement = createCard(cardData, deleteCard);
+    placesList.append(cardElement);
+  });
+}
 
-// @todo: Вывести карточки на страницу
+renderCards(initialCards);
